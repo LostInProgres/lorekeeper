f1001d6148f886706938ef429f2b087a3c8a645f
LostInProgres <mirthedh@gmail.com>
2025-01-18 00:10:43 +0100

    HMMM numbers be here

--- resources/views/gamename/index.blade.php
+++ resources/views/gamename/index.blade.php
@@ -7,6 +7,14 @@
 @section('content')
     {!! breadcrumbs(['gamename' => 'gamename']) !!}
 
+    <h1 id="sudoku_title">SUDOKU</h1>
+
+    <div id="sudoku_container"></div>
+
+    <a class="restart" href="#restart">New Game</a>
+
+    @include('gamename.style')
+
     @endsection
     @section('scripts')
         @include('gamename._game_js')
